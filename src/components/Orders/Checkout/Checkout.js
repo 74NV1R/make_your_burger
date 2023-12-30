3824cfe90eaaf86c6c6d1b6e84cdb0c4a1b464eb
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2023-12-30 19:28:10 +0600

    Spinner, order feedback added

--- src/components/Orders/Checkout/Checkout.js
+++ src/components/Orders/Checkout/Checkout.js
@@ -1,93 +1,129 @@
 import React, { Component } from 'react'
-import { Button } from 'reactstrap'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { Button, Modal, ModalBody } from 'reactstrap'
+import Spinner from '../../spinner/Spinner'
 import axios from 'axios'
+import { connect } from 'react-redux'
+import { resetIngredients } from '../../../redux/actionCreators'
+import { Link } from 'react-router-dom'
 
 const mapStateToProps = state => {
     return {
         ingredients: state.ingredients,
         totalPrice: state.totalPrice,
-        purchasable: state.purchasable
+        purchasable: state.purchasable,
     }
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+        resetIngredients: () => dispatch(resetIngredients()),
+    }
+}
 
-class Checkout extends React.Component {
+class Checkout extends Component {
     state = {
         values: {
             deliveryAddress: "",
             phone: "",
-            paymentType: "Cash on delivery",
-
-        }
+            paymentType: "Cash On Delivery",
+        },
+        isLoading: false,
+        isModalOpen: false,
+        modalMsg: "",
     }
 
-    inputChangeHandler = (e) => {
+    goBack = () => {
+        this.props.history.goBack("/");
+    }
+
+    inputChangerHandler = (e) => {
         this.setState({
             values: {
                 ...this.state.values,
-                [e.target.name]: e.target.value
+                [e.target.name]: e.target.value,
             }
         })
     }
 
     submitHandler = () => {
+        this.setState({ isLoading: true })
         const order = {
             ingredients: this.props.ingredients,
             customer: this.state.values,
             price: this.props.totalPrice,
-            orderTime: new Date()
+            orderTime: new Date(),
         }
-        //console.log(order)
-        axios.post("https://burger-f360a-default-rtdb.firebaseio.com/orders.json", order)
-            .then(response => console.log(response))
-            .catch(err => console.log(err))
+        axios.post("https://burger-f360a-default-rtdb.firebaseio.com/.json", order)
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({
+                        isLoading: false,
+                        isModalOpen: true,
+                        modalMsg: "Order Placed Successfully!",
+                    })
+                    this.props.resetIngredients()
+                } else {
+                    this.setState({
+                        isLoading: false,
+                        isModalOpen: true,
+                        modalMsg: "Something Went Wrong! Order Again!",
+                    })
+                }
+            })
+            .catch(err => {
+                this.setState({
+                    isLoading: false,
+                    isModalOpen: true,
+                    modalMsg: "Something Went Wrong! Order Again!",
+                })
+            })
     }
 
-
     render() {
+        let form = (<div>
+            <h4 style={{
+                border: "1px solid grey",
+                boxShadow: "1px 1px #888888",
+                borderRadius: "5px",
+                padding: "20px",
+            }}>Payment: {this.props.totalPrice} BDT</h4>
+            <form style={{
+                border: "1px solid grey",
+                boxShadow: "1px 1px #888888",
+                borderRadius: "5px",
+                padding: "20px",
+            }}>
+                <textarea name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Your Address" onChange={(e) => this.inputChangerHandler(e)}></textarea>
+                <br />
+                <input name="phone" className="form-control" value={this.state.values.phone} placeholder="Your Phone Number" onChange={(e) => this.inputChangerHandler(e)} />
+                <br />
+                <select name="paymentType" className="form-control" value={this.state.values.paymentType} onChange={(e) => this.inputChangerHandler(e)}>
+                    <option value="Cash On Delivery">Cash On Delivery</option>
+                    <option value="Bkash">Bkash</option>
+                </select>
+                <br />
+                <Button style={{ backgroundColor: "green" }} className="mr-auto" onClick={this.submitHandler} disabled={!this.props.purchasable}>Place Order</Button>
+
+                <Link to='/'>
+                    <Button color="secondary" className="ml-1">Cancel</Button>
+                </Link>
+
+            </form>
+        </div>)
         return (
             <div>
-                <h4 style={{
-                    border: "1px solid beige",
-                    boxShadow: "1px 1px #888888",
-                    borderRadius: "5px",
-                    padding: "10px"
-                }}> Payment: {this.props.totalPrice} BDT
-                </h4>
-                <form style={{
-                    border: "1px solid beige",
-                    boxShadow: "1px 1px #888888",
-                    borderRadius: "5px",
-                    padding: "10px"
-                }}>
-                    <textarea name='deliveryAddress' value={this.state.values.deliveryAddress} className='form-control' placeholder='your address' onChange={(e) => this.inputChangeHandler(e)}>
-                    </textarea>
-                    <br />
-                    <input name='phone' className='form-control' value={this.state.values.phone} placeholder='phone number' onChange={(e) => this.inputChangeHandler(e)} />
-                    <br />
-                    <select name='paymentType' className='form-control' value={this.state.values.paymentType} onChange={(e) => this.inputChangeHandler(e)}>
-                        <option value="Cash on delivery">Cash on delivery</option>
-                        <option value="BKash">Bkash</option>
-
-                    </select>
-                    <br />
-                    <Button style={{ backgroundColor: 'primary' }} className='mr-auto' onClick={this.submitHandler}>
-                        Place order
-                    </Button>
-
-                    <Link to="/">
-                        <Button color='danger' className='ml-1'>
-                            Cancel
-                        </Button>
-                    </Link>
-
-                </form>
-
-            </div >
+                {this.state.isLoading ? <Spinner /> : form}
+                <Modal isOpen={this.state.isModalOpen}>
+                    <ModalBody>
+                        <p>{this.state.modalMsg}</p>
+                        <Link to='/'>
+                            <Button color="secondary" className="ml-1">Go Back</Button>
+                        </Link>
+                    </ModalBody>
+                </Modal>
+            </div>
         )
     }
 }
 
-export default connect(mapStateToProps)(Checkout)
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,19 @@
 import * as actionTypes from './actionTypes'
 
-const INITIAL_STATE = {
-    ingredients: [
-        { type: 'salad', amount: 0 },
-        { type: 'cheese', amount: 0 },
-        { type: 'meat', amount: 0 }
-    ],
-    totalPrice: 80,
-    modalOpen: false,
-    purchasable: false
-}
-
 const INGREDIENT_PRICES = {
     salad: 20,
     cheese: 40,
     meat: 90,
+}
+
+const INITIAL_STATE = {
+    ingredients: [
+        { type: 'salad', amount: 0 },
+        { type: 'cheese', amount: 0 },
+        { type: 'meat', amount: 0 },
+    ],
+    totalPrice: 80,
+    purchasable: false
 }
 
 export const reducer = (state = INITIAL_STATE, action) => {
@@ -27,31 +26,41 @@ export const reducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 ingredients: ingredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload]
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload],
             }
-
         case actionTypes.REMOVE_INGREDIENT:
             for (let item of ingredients) {
                 if (item.type === action.payload) {
-                    if (item.amount <= 0) return state
-                    item.amount--
+                    if (item.amount <= 0) return state;
+                    item.amount--;
                 }
             }
             return {
                 ...state,
                 ingredients: ingredients,
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.payload]
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.payload],
             }
-
         case actionTypes.UPDATE_PURCHASABLE:
             const sum = state.ingredients.reduce((sum, element) => {
-                return sum + element.amount
+                return sum + element.amount;
             }, 0)
             return {
                 ...state,
                 purchasable: sum > 0,
             }
+        case actionTypes.RESET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: [
+                    { type: 'salad', amount: 0 },
+                    { type: 'cheese', amount: 0 },
+                    { type: 'meat', amount: 0 },
+                ],
+                totalPrice: 80,
+                purchasable: false
+            }
         default:
-            return state
+            return state;
     }
+
 }
